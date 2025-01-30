f1a47ca4474d2dc3078da2fafc3c798ea3dbb751
Harshad Kumar Damor <162619029+Harsh82092@users.noreply.github.com>
2025-01-30 14:45:30 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+    int arr[] = {12,23,13,14,24,43,26};
+    for ( int i=0;i<=arr.length-1;i++) {
+        for(int j=0;j<arr.length-i;j++){
+            if()
+        }
+    }
+
+    }
+}
